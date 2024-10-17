d1ddbfda65e039b0c03b67b29d873306b3140103
Dmitry Belyaev <dmitry.belyaev>
2024-10-17 22:59:10 +0300

    up

--- main.go
+++ main.go
@@ -5,32 +5,37 @@ import (
     "log"
     "time"
     "strconv"
-    "os"
     "math/rand"
     "sync"
     "sync/atomic"
 )
 
 const (
-    EventBusSize        = 256           // Event data bus size
-    MessageSizeMax      = 16            // Event message max length
-    PublishQueueLen     = 4<<10         // Max messages in a publish queue per Producer
-    EventBusWriteDelay  = true          // Emulate storage write acknowledged delay
+    EventBusSize            = 256           // Event data bus size
+                                            // Bigger bus size - more concurrent operations can be done
+
+    MessageSizeMax          = 16            // Event message max length, in bytes
+    PublishQueueLen         = 4<<10         // Max messages in a publish queue per Producer
+    EventBusWriteDelayUs    = 1             // Emulate storage write acknowledged delay, in microsec
+
+    // Exchange in parallel
+    Producers               = 10            // Producers countevent_data_bus
+                                            // More Producers and Consumers - more exchanges can be done
+    ProducerMessages        = 1000000       // Amount of messages per Producer
+    Consumers               = 10            // Consumers count
+    Verbose                 = false
     
-    Producers           = 10            // Producers count
-    ProducerMessages    = 1000000       // Amount of messages per Producer
-    Consumers           = 10            // Consumers count
-    Verbose             = false
-    // OR
-    //Producers           = 1
-    //ProducerMessages    = 10000000
-    //Consumers           = 1
-    //Verbose             = false
-    // OR
-    //Producers           = 2
-    //ProducerMessages    = 2
-    //Consumers           = 2
-    //Verbose             = true
+    // Exchange ~sequentially
+    //Producers               = 1
+    //ProducerMessages        = 10000000
+    //Consumers               = 1
+    //Verbose                 = false
+    
+    // Exchange with verbose
+    //Producers               = 2
+    //ProducerMessages        = 2
+    //Consumers               = 2
+    //Verbose                 = true
 )
 
 type EventBus struct {
@@ -38,20 +43,31 @@ type EventBus struct {
     publishQueues EventBusPublishQueues // Published messages ready to be read
 }
 
-type EventBusPublishQueues map[int]*chan EventBusDataKey
-
+// Map stores pointers to the data value structs (bus cells). So we can access map values
+// on read/write from many threads without need to lock map itself.
 type EventBusData map[EventBusDataKey]*EventBusDataValue
 
-type EventBusDataKey uint32
+type EventBusDataKey uint16
 
 type EventBusDataValue struct {
-    Messages []EventBusDataValueMessage
-    offsetRead int64
-    mu sync.RWMutex
+    Messages []EventBusDataValueMessage // Producer pushes messages to this list
+    offsetRead int64 // Consumer reads messages from this offset
+    mu sync.RWMutex // Producers can publish messages with equal bus keys at the same time
 }
 
-type EventBusDataValueMessage [MessageSizeMax]byte 
+// Used fixed-size bytes array to guarantee, that value will not be escaped to the heap
+type EventBusDataValueMessage [MessageSizeMax]byte
 
+// For simplicity, we use notification channels per pair Producer-Consumer. Each pair has
+// it's own notification channel.
+type EventBusPublishQueues map[int]*chan EventBusPublishQueuesChanValue
+
+type EventBusPublishQueuesChanValue struct {
+    Key EventBusDataKey
+    MsgNum int // Message num at the Producer
+}
+
+// Simple func to calculate bus key (~hash) for the message. Equal messages must have equal keys.
 func (b *EventBus) Key(msg string) EventBusDataKey {
     s := 0
     for i := 0; i < len(msg); i++ {
@@ -61,29 +77,33 @@ func (b *EventBus) Key(msg string) EventBusDataKey {
 }
 
 func main() {
-    
+
     if Producers != Consumers { // For simplicity
         log.Fatalln("Producers must be equal to Consumers")
     }
-    
+
     // Init event data bus
-    
+
+    fmt.Printf("Initializing event data bus: size: %v\n", EventBusSize)
     bus := &EventBus{Data: make(EventBusData, EventBusSize)}
     for bk := EventBusDataKey(0); bk < EventBusDataKey(EventBusSize); bk++ {
         bus.Data[bk] = &EventBusDataValue{
-            Messages: make([]EventBusDataValueMessage, 0, PublishQueueLen),
+            Messages: make([]EventBusDataValueMessage, 0, ProducerMessages),
         }
     }
-    
+
+    // Initializing notification channels per pair Producer-Consumer
     bus.publishQueues = make(EventBusPublishQueues, Producers)
     for p := 0; p < Producers; p++ {
-        ch := make(chan EventBusDataKey, PublishQueueLen)
+        ch := make(chan EventBusPublishQueuesChanValue, PublishQueueLen)
         bus.publishQueues[p] = &ch
     }
-    fmt.Printf("Event data bus inited: size: %v\n", EventBusSize)
-    
-    // Generate messages
 
+    // Generate messages
+    // Prepare messages for write-read to not waste time on this during exchange process
+
+    messagesTotal := Producers * ProducerMessages
+    fmt.Printf("Generating messages: total: %v\n", messagesTotal)
     rand.Seed(time.Now().UTC().UnixNano())
     messages := make([][]string, Producers)
     for p := 0; p < Producers; p++ {
@@ -91,15 +111,19 @@ func main() {
         for m := 0; m < ProducerMessages; m++ {
             // Message to publish
             messages[p][m] = "m_" +
-                strconv.Itoa(rand.Intn(1000)) + "_" + 
-                strconv.Itoa(rand.Intn(1000)) + "_" + 
+                strconv.Itoa(rand.Intn(1000)) + "_" +
+                strconv.Itoa(rand.Intn(1000)) + "_" +
                 strconv.Itoa(rand.Intn(1000))
+            if l := len(messages[p][m]); l > MessageSizeMax {
+                log.Fatalf("Bad message size: %v", l)
+            }
         }
     }
-    fmt.Printf("Generated messages: total: %v\n", Producers * ProducerMessages)
-    
+
     // Calculate messages bus keys (~ message hash)
-    
+    // Prepare keys to not waste time on this during exchange process
+
+    fmt.Printf("Calculating messages bus keys\n")
     messagesKeys := make([][]EventBusDataKey, Producers)
     for p := 0; p < Producers; p++ {
         messagesKeys[p] = make([]EventBusDataKey, ProducerMessages)
@@ -107,72 +131,94 @@ func main() {
             messagesKeys[p][m] = bus.Key(messages[p][m])
         }
     }
-    fmt.Printf("Calculated messages bus keys\n")
-    
+
     // Run
-    
+
     fmt.Printf("Producers: %v\n", Producers)
     fmt.Printf("Consumers: %v\n", Consumers)
-    
+
     fmt.Printf("Sending messages ...\n")
-    
+
     timeStart := time.Now()
-    
+
     // Producers
-    
+
     for p := 0; p < Producers; p++ {
         p := p // for compatibility with older versions of golang
-        go func() {
-            for m := 0; m < ProducerMessages; m++ {
 
-                msg := messages[p][m] // Message to publish
-                bk := messagesKeys[p][m] // Message bus key
-                
-                bus.Data[bk].mu.Lock()
-                
+        go func() { // Run Producer
+            for m := 0; m < ProducerMessages; m++ { // Publish messages (write to the Bus)
+
+                msg := messages[p][m] // Message to publish (write)
+                bk := messagesKeys[p][m] // Bus key
+                bv := bus.Data[bk] // Pointer to bus value (cell). We access map only on read.
+
                 var mbyt EventBusDataValueMessage
-                copy(mbyt[:], msg)
-                bus.Data[bk].Messages = append(bus.Data[bk].Messages, mbyt)
-                
-                if EventBusWriteDelay {
-                    time.Sleep(time.Microsecond * 1)
-                }
-                
-                if Verbose {
-                    fmt.Printf("producer[%v][%v] bk[%v]:\t%v\n", p, m, bk, msg)
-                }
-                bus.Data[bk].mu.Unlock()
-                
-                *bus.publishQueues[p] <- bk
+                copy(mbyt[:], msg) // Convert string to array of bytes
+
+                func() { // Use local func and defer unlock to guarantee that unlock will be done
+                    bv.mu.Lock()
+                    defer bv.mu.Unlock()
+
+                    bv.Messages = append(bv.Messages, mbyt) // Write message to the bus
+
+                    if EventBusWriteDelayUs > 0 { // Emulate write delay
+                        time.Sleep(time.Microsecond * EventBusWriteDelayUs)
+                    }
+                    if Verbose {
+                        fmt.Printf("producer[%v]: msgNum: %v, busKey: %v, msg: %v\n", p, m, bk, msg)
+                    }
+                }()
+
+                // Notify Consumer that bus was written
+                *bus.publishQueues[p] <- EventBusPublishQueuesChanValue{Key: bk, MsgNum: m}
             }
             close(*bus.publishQueues[p])
         }()
     }
-    
+
     // Consumers
-    
+
     var wgConsumers sync.WaitGroup
     wgConsumers.Add(Consumers)
-    
+
     for c := 0; c < Consumers; c++ {
         c := c // for compatibility with older versions of golang
-        go func() {
+
+        go func() { // Run consumer
             defer wgConsumers.Done()
-            for bk := range *bus.publishQueues[c] {
-                bus.Data[bk].mu.RLock()
-                if Verbose {
-                    fmt.Printf("consumer[%v][-] bk[%v]:\t%v\n", c, bk, 
-                               string(bus.Data[bk].Messages[ bus.Data[bk].offsetRead ][:]))
-                }
-                atomic.AddInt64(&bus.Data[bk].offsetRead, 1)
-                bus.Data[bk].mu.RUnlock()
+
+            for pub := range *bus.publishQueues[c] { // Read as soon as bus write event occurred
+
+                bk := pub.Key // Bus key
+                bv := bus.Data[bk] // Pointer to bus value (cellevent_data_bus). We access map only on read.
+
+                func() { // Use local func and defer unlock to guarantee that unlock will be done
+                    bv.mu.RLock()
+                    defer bv.mu.RUnlock()
+
+                    // Use atomic, as we have only read-lock here
+                    offset := atomic.LoadInt64(&bv.offsetRead)
+                    atomic.AddInt64(&bv.offsetRead, 1) // offsetRead++
+                    //runtime.Gosched() // Not need
+
+                    msg := string(bv.Messages[offset][:]) // Read message from the bus
+
+                    if Verbose {
+                        fmt.Printf("consumer[%v]: msgNum: %v, busKey: %v, msg: %v\n", c, pub.MsgNum, bk, msg)
+                    }
+                }()
             }
         }()
     }
-    
-    wgConsumers.Wait()
-    
+    wgConsumers.Wait() // Wait till Consumers have read all messages
+
+    // Result
     timeTaken := time.Since(timeStart).Milliseconds()
+    rps := 0
+    if timeTaken > 0 {
+        rps = int(float64(messagesTotal) / (float64(int(timeTaken)) / 1000))
+    }
+    fmt.Printf("RPS: %v\n", rps)
     fmt.Printf("Time taken: %v ms\n", timeTaken)
-    os.Exit(0)
 }
